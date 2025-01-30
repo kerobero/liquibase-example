3958aa65e2d682eda6816d7f6bd32a0c21a05c4c
Juan Pablo Castrillón <kerobero@sdf.org>
2025-01-30 02:43:19 +0000

    Change function to increment two

--- DDL/functions.sql
+++ DDL/functions.sql
@@ -1,5 +1,5 @@
 -- liquibase formatted sql
 -- changeset changelog:fn_increment splitStatements:false runOnChange:true
-CREATE OR REPLACE FUNCTION increment(i integer) RETURNS integer AS $$ BEGIN RETURN i + 1;
+CREATE OR REPLACE FUNCTION increment(i integer) RETURNS integer AS $$ BEGIN RETURN i + 2;
 END;
 $$ LANGUAGE plpgsql;
